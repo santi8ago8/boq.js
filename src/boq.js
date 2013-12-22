a1c37c472e711e3b1f8a1b6fc11db3a0f320ef63
santi8ago8 <santi8ago8@gmail.com>
2013-12-22 02:58:37 -0300

    agregando la clase Array, por ahora tiene each y reverse

--- src/boq.js
+++ src/boq.js
@@ -21,7 +21,7 @@
         debug: function (args) {
             //control for internet explorer
             if (typeof console !== 'undefined') {
-                console.debug(arguments.length == 1 ? arguments[0] : args);
+                console.debug(arguments.length == 1 ? arguments[0] : arguments);
             }
         },
         /**
@@ -31,7 +31,7 @@
         log: function (args) {
             //control for internet explorer
             if (typeof console !== 'undefined') {
-                console.log(arguments.length == 1 ? arguments[0] : args);
+                console.log(arguments.length == 1 ? arguments[0] : arguments);
             }
         },
         /**
@@ -77,12 +77,70 @@
          * get a random integer number
          * @param {number} [min] minimun value
          * @param {number} [max] maximun value
-         * @returns {number}
+         * @returns {number} a random int number
          */
         randomInt: function (min, max) {
             return parseInt(boq.utils.random(min, max));
         }
     };
+
+    /**
+     * create a boq array
+     * @param {Array} [arr=[]] origin array
+     * @constructor
+     */
+    boq.Array = function (arr) {
+        var self = typeof arr !== 'undefined' ? arr : [];
+
+        /**
+         * each in the array
+         * @param {function} fn iteration function
+         * @param {number} [start=0]
+         * @param {number} [interval=1]
+         * @returns {boq.Array}
+         */
+        self.each = function (fn, start, interval) {
+            if (typeof fn !== 'function') {
+                interval = start;
+                start = fn;
+                fn = false;
+            }
+            if (typeof start == 'undefined')
+                start = 0;
+            if (typeof interval == 'undefined')
+                interval = 1;
+
+            var result = new boq.Array();
+            for (var i = start;
+                //normal for
+                 (interval > 0 && i < self.length) ||
+                     //reverse for, used to reverse function.
+                     (interval < 0 && i >= 0)
+
+                ; i += interval) {
+                var res = true;
+                if (fn) {
+                    res = fn(self[i], i);
+                }
+                if (res || typeof res === 'undefined') {
+                    result.push(self[i]);
+                }
+
+            }
+            return result;
+        };
+
+        /**
+         * reverse array
+         * @returns {boq.Array}
+         */
+        self.reverse = function(){
+            return self.each(self.length-1,-1);
+        };
+
+        return self;
+    };
+
 
     window.Boq = window.boq = boq;
 }(window));
